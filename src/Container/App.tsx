a5ca773581ff6ea4ffb6a01f7e5e9974cfb079c0
oleksandr-pavliuk <61949378+FoldeR-ua@users.noreply.github.com>
2023-09-27 08:57:53 +0300

    created react-app added home page footer header

---
+++ src/Container/App.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import {Header, Footer} from "../Components/Layout";
+import {useState, useEffect} from 'react';
+import { menuItemModel } from '../Interfaces';
+import {Home} from '../Pages';
+
+
+
+function App() {
+  
+  return (
+    <div>
+      <Header/>
+        <Home/>
+      <Footer/>
+    </div>
+  );
+}
+
+export default App;
